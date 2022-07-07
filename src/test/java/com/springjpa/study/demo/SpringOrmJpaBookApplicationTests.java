3d78f0a79dba7b48cc70947934aec1c05a9b8921
shinjisub <spdlqjaksemf@naver.com>
2022-07-07 23:47:55 +0900

    Initial commit

---
+++ src/test/java/com/springjpa/study/demo/SpringOrmJpaBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springjpa.study.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOrmJpaBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
